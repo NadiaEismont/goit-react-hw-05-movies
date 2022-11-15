60ec5a0208f73c822ad296430119e1ba27383cf4
Nadia Nechuiviter <nadiaejsmont@gmail.com>
2022-11-15 19:34:33 +0200

    try popover

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -3,7 +3,15 @@ import { useSearchMovie } from '../hooks/trendingMoviesHook';
 import { useMovieDetails } from '../hooks/MoviesDetails';
 import { getMoviesDetails, getMoviesCredits, getMoviesReviews } from '../API';
 import { Outlet, useLocation } from 'react-router-dom';
-import { Pane, Text, Heading, HandLeftIcon, Icon } from 'evergreen-ui';
+import {
+  Pane,
+  Text,
+  Heading,
+  HandLeftIcon,
+  Icon,
+  minorScale,
+   Popover
+} from 'evergreen-ui';
 
 export const MovieDetails = () => {
   const { movieId } = useParams();
@@ -21,21 +29,30 @@ export const MovieDetails = () => {
           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           alt=""
         />
-        <Heading size={400}>
+        <Heading size={600}>
           {movie.title}({movie.release_date})
         </Heading>
-        <Text>User score: {movie.vote_average}</Text>
-        <Heading size={400}>Overview</Heading>
-        <Text>{movie.overview}</Text>
-        <Heading size={400}>Genres</Heading>
-        <p>{movie.genres && movie.genres.map(gen => gen.name + ' ')}</p>
+        <Text size={600}>User score: {movie.vote_average}</Text>
+        <Heading size={600}>Overview</Heading>
+        <Text size={600}>{movie.overview}</Text>
+        <Heading size={600}>Genres</Heading>
+        <Text size={600} marginBottom={80}>
+          {movie.genres && movie.genres.map(gen => gen.name + ', ')}
+        </Text>
         <Pane>
           <Heading size={700}>Additional information</Heading>
+           <Popover  content={
           <Link to="cast" state={{ from: location }}>
-            Cast
+            <Text size={minorScale(1)} marginRight={30}>
+              Cast
+              </Text> }>
+               <Button>Trigger Popover</Button>
+             </Popover>
           </Link>
           <Link to="reviews" state={{ from: location }}>
-            Reviews
+            <Text size={minorScale(1)} marginRight={30}>
+              Reviews
+            </Text>
           </Link>
           <Outlet />
         </Pane>
